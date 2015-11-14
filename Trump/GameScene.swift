26faad4f42fd587b1bc88a3260fbdefcd04db3ac
ColeHud <cmhudson11@gmail.com>
2015-11-14 11:02:33 -0800

    hair stuff is kind of broken

--- Trump/GameScene.swift
+++ Trump/GameScene.swift
@@ -17,6 +17,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     var contentCreated = false
     var motionManager: CMMotionManager!
     
+    var myView: SKView?
+    
     // Object Lifecycle Management
     
     // Scene Setup and Content Creation
@@ -26,6 +28,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         motionManager.startAccelerometerUpdates()
         if (!self.contentCreated)
         {
+            myView = view
             self.createContent()
             self.contentCreated = true
         }
@@ -35,6 +38,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate
     {
         let trump = SKSpriteNode(imageNamed: "noHair.png")
         
+        if let theViewWidth = myView?.bounds.size.width { //THIS IS GOOD CODE :)
+            
+            trump.size.height = trump.size.height * (theViewWidth / trump.size.width)
+            
+            trump.size.width = theViewWidth
+            
+        } else {
+            //handle the calc if nil
+        }
+        
         trump.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
         
         self.addChild(trump)
@@ -43,7 +56,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         self.backgroundColor = UIColor.init(colorLiteralRed: 24.0, green: 48.0, blue: 89.0, alpha: 1.0)
         
         setUpPhysics()
-        setUpHair()
+        setUpHair(trump)
     }
     
     // Scene Update
@@ -65,10 +78,49 @@ class GameScene: SKScene, SKPhysicsContactDelegate
         */
     }
     
-    private func setUpHair()
+    private func setUpHair(trump: SKSpriteNode)
     {
-        let length = 7 * Int(UIScreen.mainScreen().scale)
-        let relAnchorPoint = CGPointMake(self.size.width/2, self.size.height/2)
+        let length = 14 * Int(UIScreen.mainScreen().scale)
+        //let relAnchorPoint = CGPointMake(self.size.width/2, self.size.height/2)
+        var relAnchorPoint = CGPointMake(trump.size.width/2, trump.size.height/2)
+        
+        //get all pixels above a certain line that isn't white
+        if #available(iOS 9.0, *) {
+            var image = (trump.texture?.CGImage)!
+            var bitmapData = CGDataProviderCopyData(CGImageGetDataProvider(image))
+            var data = CFDataGetBytePtr(bitmapData);
+            let bytesPerRow = CGImageGetBytesPerRow(image)
+            let width = CGImageGetWidth(image)
+            let height = CGImageGetHeight(image);
+            
+            var rawData = CGDataProviderCopyData(CGImageGetDataProvider(image));
+            var bufptr = CFDataGetBytePtr(rawData);
+            
+            var hairs = 0
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++ )
+                {
+                    let alpha = bufptr[3]// components of each pixel
+                
+                    if(alpha == 255 && CGFloat(y) < UIImage(CGImage: image).size.height/5 && hairs < 15 && arc4random_uniform(10) == 1)
+                    {
+                        let floatx = CGFloat(x)
+                        let floaty = CGFloat(y)
+                        
+                        relAnchorPoint = CGPointMake(floatx, floaty)
+                        let strand = HairNode(length: length, anchorPoint: relAnchorPoint, name: "Hairstrand")
+                        strand.addToScene(self)
+                        hairs++
+                    }
+                
+                    bufptr += 4;
+                }
+            }
+            
+        } else {
+            // Fallback on earlier versions
+        }
         
         let strand = HairNode(length: length, anchorPoint: relAnchorPoint, name: "Hairstrand")
         strand.addToScene(self)
--- Trump/HairNode.swift
+++ Trump/HairNode.swift
@@ -51,7 +51,7 @@ class HairNode: SKNode
         scene.addChild(self)
         
         // create rope holder
-        let hairHolder = SKSpriteNode(imageNamed: "HairHolder")
+        let hairHolder = SKSpriteNode(imageNamed: "HairHolder2")
         hairHolder.position = anchorPoint
         hairHolder.zPosition = Layer.Hair
         
@@ -68,8 +68,8 @@ class HairNode: SKNode
         for i in 0..<length {
             
             let hairSegment = SKSpriteNode(imageNamed: "craziestHair")
-            hairSegment.size.height = hairSegment.size.height/4
-            hairSegment.size.width = hairSegment.size.width/5
+            hairSegment.size.height = hairSegment.size.height/10
+            hairSegment.size.width = hairSegment.size.width/6
             let offset = hairSegment.size.height * CGFloat(i + 1)
             hairSegment.position = CGPointMake(anchorPoint.x, anchorPoint.y - offset)
             hairSegment.name = name
